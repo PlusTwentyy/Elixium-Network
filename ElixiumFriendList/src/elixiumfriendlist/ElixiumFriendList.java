6076456ee74d7fca81af403387e62a726387a9b8
InfernoGalaxy <jade.blackmon58@gmail.com>
2019-01-25 23:17:32 -0500

    First Commit

---
+++ ElixiumFriendList/src/elixiumfriendlist/ElixiumFriendList.java
@@ -0,0 +1,16 @@
+package elixiumfriendlist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ElixiumFriendList extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
